74e98d92931b2cfa1564cbafadc8a50ba9d210b8
Ayoub <ayoubzoubeidi@gmail.com>
2023-07-29 10:36:59 +0100

    Initial project commit.

---
+++ src/main/java/tn/maz/jakartaeetest/HelloApplication.java
@@ -0,0 +1,9 @@
+package tn.maz.jakartaeetest;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
